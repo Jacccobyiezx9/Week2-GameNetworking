2dfaee7dd2eeb6e27214a84221a3a9dbaac01e17
Jacccobyiezx9 <167274314+Jacccobyiezx9@users.noreply.github.com>
2026-01-22 20:23:01 +0800

    Added Color changing and authority

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -4,11 +4,24 @@ using Fusion;
 public class NetworkPlayer : NetworkBehaviour
 {
     [SerializeField] private MeshRenderer m_MeshRenderer;
+
+    [Header("Networked Properties")]
+    [Networked] public Vector3 NetworkedPosition { get; set; }
+    [Networked] public Color PlayerColor { get; set; }
     #region Fusion Callbacks
+
     //Initialization Logic (New Start/Awake)
     public override void Spawned()
     {
-        
+        if (HasInputAuthority) //client
+        {
+
+        }
+
+        if (HasStateAuthority) //server
+        {
+            PlayerColor = Random.ColorHSV();
+        }
     }
 
     //OnDestroy
@@ -20,19 +33,50 @@ public class NetworkPlayer : NetworkBehaviour
     //Update
     public override void FixedUpdateNetwork()
     {
+        if (!HasStateAuthority) return;
         if(GetInput(out NetworkInputData input))
         {
             this.transform.position += 
                 new Vector3(input.InputVector.normalized.x, input.InputVector.normalized.y)
                 * Runner.DeltaTime;
+
+            NetworkedPosition = this.transform.position;
         }
     }
 
-    //Happens FixedUpdateNetwork, for non server objects
+    //Happens after FixedUpdateNetwork, for non server objects
     public override void Render()
     {
-        
+        this.transform.position = NetworkedPosition;
+        if(m_MeshRenderer != null && m_MeshRenderer.material.color != PlayerColor)
+        {
+            m_MeshRenderer.material.color = PlayerColor;
+        }
+            
+    }
+
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)] 
+    private void RPC_SetPlayerColor(Color color)
+    {
+        if (HasStateAuthority)
+        {
+            this.PlayerColor = color;
+        }
+            
     }
     #endregion
 
+
+    #region UnityCallbacks
+    private void Update()
+    {
+        if (!HasInputAuthority) return;
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            var randColor = Random.ColorHSV();
+            RPC_SetPlayerColor(randColor);
+        }
+    }
+    #endregion
 }
